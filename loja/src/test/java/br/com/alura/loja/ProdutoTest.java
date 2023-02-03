c70dfb5a0fb3ebaed5006afa6ffe7b9eb183ceaa
henrique.l.araujo <henrique.l.araujo@ericssoninovacao.com.br>
2023-02-03 17:05:10 -0300

    first commit

---
+++ loja/src/test/java/br/com/alura/loja/ProdutoTest.java
@@ -0,0 +1,17 @@
+package br.com.alura.loja;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class ProdutoTest {
+
+    @Test
+    public void test() {
+        Produto produto = new Produto("teste", BigDecimal.TEN);
+        Assert.assertEquals("teste", produto.getNome());
+        Assert.assertEquals(BigDecimal.TEN, produto.getPreco());
+    }
+
+}
